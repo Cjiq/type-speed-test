c0d0b164172050c3c5da10d52fec637d47655581
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2018-03-21 10:29:36 +0100

    Start button click listener

---
+++ index.js
@@ -0,0 +1,6 @@
+const startBtn = document.getElementById("start-button")
+if (startBtn) startBtn.addEventListener("click", () => startGame())
+
+function startGame() {
+  console.log("Game started!")
+}
