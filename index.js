26abc4869f1532495fe4ad58fb707786d075f5cf
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2018-03-21 11:20:56 +0100

    Implemented count down timeer

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const startBtn = document.getElementById("start-button")
 const board = document.getElementById("game-board")
 const textContainer = document.getElementById("text-container")
+const timerElem = document.getElementById("timer")
 
 if (startBtn) startBtn.addEventListener("click", (e) => startGame(e))
 
 function startGame(e) {
   e.target.style.display = "none";
   board.style.display = "block";
-  const timer = startTimer();
+  
+  startTimer();
 }
 
 function startTimer() {
   const startTime = new Date().getTime()
-  return setInterval(() => {
+  const timer = setInterval(() => {
       const now = new Date().getTime()
-      const seconds = Math.floor(((now - startTime) % (1000 * 60)) / 1000)
-      
+      const seconds = 60 - Math.floor(((now - startTime) % (1000 * 60)) / 1000)
+      timerElem.innerHTML = `${seconds} s`
+      if (seconds === 0)  exitGame(timer)
   })
+}
+
+function exitGame(timer) {
+  clearInterval(timer)
 }
