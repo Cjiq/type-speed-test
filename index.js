107728b186d5259ef8a412575f02d7d1da28ad97
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2018-03-21 11:11:05 +0100

    Start count down implementation

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const startBtn = document.getElementById("start-button")
-if (startBtn) startBtn.addEventListener("click", () => startGame())
+const board = document.getElementById("game-board")
+const textContainer = document.getElementById("text-container")
 
-function startGame() {
-  console.log("Game started!")
+if (startBtn) startBtn.addEventListener("click", (e) => startGame(e))
+
+function startGame(e) {
+  e.target.style.display = "none";
+  board.style.display = "block";
+  const timer = startTimer();
+}
+
+function startTimer() {
+  const startTime = new Date().getTime()
+  return setInterval(() => {
+      const now = new Date().getTime()
+      const seconds = Math.floor(((now - startTime) % (1000 * 60)) / 1000)
+      
+  })
 }
