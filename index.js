912ecac6c2571f41c60c696a955cd05facc4f7dc
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2018-03-21 11:55:16 +0100

    Add word functionality

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-const startBtn = document.getElementById("start-button")
-const board = document.getElementById("game-board")
-const textContainer = document.getElementById("text-container")
-const timerElem = document.getElementById("timer")
+const startBtn = document.getElementById("start-button");
+const board = document.getElementById("game-board");
+const textContainer = document.getElementById("text-container");
+const timerElem = document.getElementById("timer");
 
-if (startBtn) startBtn.addEventListener("click", (e) => startGame(e))
+startBtn.addEventListener("click", () => setupGame());
 
-function startGame(e) {
-  e.target.style.display = "none";
-  board.style.display = "block";
-  
+function setupGame() {
+  showBoard();
   startTimer();
+  asCharSequence(getRandomWord(5)).forEach(char => {
+    textContainer.appendChild(asDomElem(char));
+  })
+}
+
+function showBoard() {
+  startBtn.style.display = "none";
+  board.style.display = "block";
 }
 
 function startTimer() {
-  const startTime = new Date().getTime()
+  const startTime = new Date().getTime();
   const timer = setInterval(() => {
-      const now = new Date().getTime()
-      const seconds = 60 - Math.floor(((now - startTime) % (1000 * 60)) / 1000)
-      timerElem.innerHTML = `${seconds} s`
-      if (seconds === 0)  exitGame(timer)
+      const now = new Date().getTime();
+      const seconds = 60 - Math.floor(((now - startTime)) / 1000);
+      timerElem.innerHTML = `${seconds} s`;
+      if (seconds === 0)  exitGame(timer);
   })
 }
 
 function exitGame(timer) {
-  clearInterval(timer)
+  clearInterval(timer);
+}
+
+function getRandomWord(n = 1) {
+  return Array.from({length: n}, 
+    () => Math.floor(Math.random() * wordList.length))
+    .map(i => `${wordList[i]} `);
+}
+
+function asCharSequence(warr) {
+  return [].concat.apply([], warr.map(w => w.split('')));
+}
+
+function asDomElem(char) {
+  const div = document.createElement("div");
+  div.classList.add("char");
+  if (char === " ") div.classList.add("space")
+  div.innerText = char;
+  return div;
 }
