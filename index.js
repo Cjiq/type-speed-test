a2745a05f1694483f02fc95ef4ff0a7cd7387234
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2018-03-22 20:58:57 +0100

    Most of the code done

--- index.js
+++ index.js
@@ -1,16 +1,106 @@
-const startBtn = document.getElementById("start-button");
+﻿const startBtn = document.getElementById("start-button");
 const board = document.getElementById("game-board");
 const textContainer = document.getElementById("text-container");
 const timerElem = document.getElementById("timer");
+const accuracy = document.getElementById("accuracy");
 
 startBtn.addEventListener("click", () => setupGame());
+document.addEventListener("keydown", (e) => keyPressHandler(e));
+
+const wordSequences = [];
+const charElems = [];
 
 function setupGame() {
   showBoard();
   startTimer();
-  asCharSequence(getRandomWord(5)).forEach(char => {
-    textContainer.appendChild(asDomElem(char));
+  newWordSequence();
+}
+
+function newWordSequence() {
+  textContainer.innerHTML = "";
+  charElems.length = 0; /* Don't argue with the standard :P */
+  const words = getRandomWord(5);
+  wordSequences.push(words);
+  
+  asCharSequence(words).forEach(char => {
+    const elem = asDomElem(char);
+    charElems.push(elem);
+    textContainer.appendChild(elem);
   })
+
+  charElems[index = 0].classList.add("active");  
+}
+
+let corrects = [];
+let numCorrect = 0;
+let numError = 0;
+let index = 0;
+function keyPressHandler(event) {
+  if (nonRelevantKey(event)) return;
+  if (isEndOfWords()) {
+    newWordSequence();
+    return;
+  }
+
+  const elem = charElems[index];
+  elem.classList.remove("active");  
+  
+  if (event.key === "Backspace") {
+    moveBackwards();
+  } else {
+    const wasCorrect = (event.key === elem.innerText) || (event.key === " " && elem.innerText === "\u00A0");
+    moveForward(wasCorrect);
+  }
+
+  accuracy.innerText = `${Math.floor(numCorrect / (numCorrect + numError) * 100) || 0}%`
+}
+
+function nonRelevantKey(event) {
+  if (event.ctrlKey) return true;
+  if (event.shiftKey) return true;
+  if (event.key === "Alt") return true;
+  if (event.key === "ArrowLeft") return true;
+  if (event.key === "ArrowUp") return true;
+  if (event.key === "ArrowRight") return true;
+  if (event.key === "ArrowDown") return true;
+  if (event.key === "Meta") return true;
+  if (event.key === "Dead") return true;
+  return false;
+}
+
+function isEndOfWords() {
+  return asCharSequence(wordSequences.slice(-1).pop()).length - 1 === index;
+}
+
+function moveBackwards() {
+  if (index === 0) {
+    charElems[index].classList.add("active");
+    return;
+  }
+
+  const elem = charElems[--index];
+    if (corrects.pop()) {
+      elem.classList.remove("correct");
+      numCorrect--;
+    } 
+    else {
+      elem.classList.remove("error");
+      numError--;
+    }
+    elem.classList.add("active");
+}
+
+function moveForward(correct) {
+  if (correct) {
+    numCorrect++;
+    corrects.push(correct);
+    charElems[index].classList.add("correct");
+  } else {
+    numError++;
+    corrects.push(correct);
+    charElems[index].classList.add("error");
+  }
+  charElems[++index].classList.add("active");
 }
 
 function showBoard() {
@@ -35,7 +125,7 @@ function exitGame(timer) {
 function getRandomWord(n = 1) {
   return Array.from({length: n}, 
     () => Math.floor(Math.random() * wordList.length))
-    .map(i => `${wordList[i]} `);
+    .map(i => `${wordList[i]}\u00A0`);
 }
 
 function asCharSequence(warr) {
@@ -45,7 +135,6 @@ function asCharSequence(warr) {
 function asDomElem(char) {
   const div = document.createElement("div");
   div.classList.add("char");
-  if (char === " ") div.classList.add("space")
   div.innerText = char;
   return div;
 }
